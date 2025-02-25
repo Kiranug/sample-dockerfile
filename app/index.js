6af56c3dce3a04e0c8b555207003f72c6bb3c6c0
Kiranug <kiran.ug123@gmail.com>
2025-02-25 12:57:26 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 80;  // Set port to 80
 
 // Basic route
 app.get("/", (req, res) => {
